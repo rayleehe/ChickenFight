229b7733d66b3947f2c6d2769012c54cbe870635
Tushar Chopra <tusharchopra17@gmail.com>
2018-10-24 17:08:26 -0700

    started implenting multipeer connectivity

--- ChickenFight/ViewController.swift
+++ ChickenFight/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import SceneKit
 import ARKit
+import MultipeerConnectivity
 
 class ViewController: UIViewController, ARSCNViewDelegate {
     
@@ -19,8 +20,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     var hasTouched = false
     var player1 = SCNNode()
     
+    var multipeerSession = MultipeerSession!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        multipeerSession = MultipeerSession(receivedDataHandler: receivedData)
         
         // Set the view's delegate
         sceneView.delegate = self
@@ -48,68 +53,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
     
-    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-        if !didAddNode{
-            guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
-            
-            // 2
-            let width = CGFloat(planeAnchor.extent.x)
-            let height = CGFloat(planeAnchor.extent.z)
-            let plane = SCNBox(width: width, height: 0.001, length: height, chamferRadius: 0)
-            
-            // 3
-            plane.materials.first?.diffuse.contents = UIColor.blue.withAlphaComponent(0.7)
-            
-            // 4
-            var planeNode = SCNNode(geometry: plane)
-            
-            // 5
-            let x = CGFloat(planeAnchor.center.x)
-            let y = CGFloat(planeAnchor.center.y)
-            let z = CGFloat(planeAnchor.center.z)
-            planeNode.position = SCNVector3(x,y,z)
-            
-            // 6
-            main = node
-            node.addChildNode(planeNode)
-            planeNode.physicsBody = SCNPhysicsBody(type: .static, shape: nil)
-            didAddNode = true
-        }
-    }
     
-    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
-        if !hasTouched{
-            guard let planeAnchor = anchor as?  ARPlaneAnchor,
-                var planeNode = node.childNodes.first,
-                let plane = planeNode.geometry as? SCNBox
-                else { return }
-            
-            // 2
-            let width = CGFloat(planeAnchor.extent.x)
-            let height = CGFloat(planeAnchor.extent.z)
-            plane.width = width
-            plane.length = height
-            
-            // 3
-            let x = CGFloat(planeAnchor.center.x)
-            let y = CGFloat(planeAnchor.center.y)
-            let z = CGFloat(planeAnchor.center.z)
-            planeNode.position = SCNVector3(x, y, z)
-            planePosition = planeNode.position
-        }
-    }
-    
-    
-    // MARK: - ARSCNViewDelegate
-    
-    /*
-     // Override to create and configure nodes for anchors added to the view's session.
-     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-     let node = SCNNode()
-     
-     return node
-     }
-     */
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         let box = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
         box.materials.first?.diffuse.contents = UIColor.red
@@ -130,8 +74,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         if point.x < view.frame.width / 2 {
             player1.physicsBody?.applyForce(SCNVector3(0, 3, 0), asImpulse: true)
         } else {
-            print(player1.physicsBody?.type)
-            
+    
         }
         
         hasTouched = true
@@ -149,5 +92,76 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if !didAddNode{
+            guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+            
+            let width = CGFloat(planeAnchor.extent.x)
+            let height = CGFloat(planeAnchor.extent.z)
+            let plane = SCNBox(width: width, height: 0.001, length: height, chamferRadius: 0)
+            
+            plane.materials.first?.diffuse.contents = UIColor.blue.withAlphaComponent(0.7)
+            
+            var planeNode = SCNNode(geometry: plane)
+            
+            let x = CGFloat(planeAnchor.center.x)
+            let y = CGFloat(planeAnchor.center.y)
+            let z = CGFloat(planeAnchor.center.z)
+            planeNode.position = SCNVector3(x,y,z)
+            
+            
+            main = node
+            node.addChildNode(planeNode)
+            planeNode.physicsBody = SCNPhysicsBody(type: .static, shape: nil)
+            didAddNode = true
+        }
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        if !hasTouched{
+            guard let planeAnchor = anchor as?  ARPlaneAnchor,
+                var planeNode = node.childNodes.first,
+                let plane = planeNode.geometry as? SCNBox
+                else { return }
+            
+            let width = CGFloat(planeAnchor.extent.x)
+            let height = CGFloat(planeAnchor.extent.z)
+            plane.width = width
+            plane.length = height
+            
+            let x = CGFloat(planeAnchor.center.x)
+            let y = CGFloat(planeAnchor.center.y)
+            let z = CGFloat(planeAnchor.center.z)
+            planeNode.position = SCNVector3(x, y, z)
+            planePosition = planeNode.position
+        }
+    }
+    
+    func receivedData(_ data: Data, from peer: MCPeerID) {
+        
+        do {
+            if let worldMap = try NSKeyedUnarchiver.unarchivedObject(ofClass: ARWorldMap.self, from: data) {
+                // Run the session with the received world map.
+                let configuration = ARWorldTrackingConfiguration()
+                configuration.planeDetection = .horizontal
+                configuration.initialWorldMap = worldMap
+                sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+                
+                // Remember who provided the map for showing UI feedback.
+                mapProvider = peer
+            }
+            else
+                if let anchor = try NSKeyedUnarchiver.unarchivedObject(ofClass: ARAnchor.self, from: data) {
+                    // Add anchor to the session, ARSCNView delegate adds visible content.
+                    sceneView.session.add(anchor: anchor)
+                }
+                else {
+                    print("unknown data recieved from \(peer)")
+            }
+        } catch {
+            print("can't decode data recieved from \(peer)")
+        }
     }
 }
